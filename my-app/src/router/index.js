a7f2e02a1442a24721e00e1b2cfd25aa3a20c5f9
Сергей Малюгов <gr402_maspe@lab-407-4.tomtit-tomsk.ru>
2024-01-19 12:51:05 +0700

    создан login

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -1,27 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store/index.js'
 
+const ifNotAuthenticated = (to, from, next) =>{
+  if(!store.getters.isAuthenticated){
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) =>{
+  if(store.getters.isAuthenticated){
+    next();
+    return;
+  }
+  next('/login');
+};
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: function(){
+      return import('../views/HomeView.vue');
+    },
+    beforeEnter: ifAuthenticated,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    path: '/login',
+    name: 'login',
+    component: function(){
+      return import('../components/Login.vue');
+    },
+    beforeEnter: ifNotAuthenticated,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
 
 export default router
